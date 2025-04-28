6cdb3449330c1230c3b21e2449966e58c153e44c
PP <908@.com>
2025-04-28 12:00:23 +0800

    使用com的形式

--- Editor/ScriptCreater/GeneratorCustomScriptFile.cs
+++ Editor/ScriptCreater/GeneratorCustomScriptFile.cs
@@ -32,7 +32,7 @@ namespace HoopyGame.Editor
         private const string assetScriptTempatePath =
             "Assets/com.hoopygame.hoopygameframework/Editor/ScriptCreater/Template/";
         private const string packageScriptTempatePath =
-            "Packages/HoopyGame Framework/Editor/ScriptCreater/Template/";
+            "Packages/com.hoopygame.hoopygameframework/Editor/ScriptCreater/Template/";
 
         [MenuItem("Assets/Create/Noraml C#", false, 60)]
         public static void GeneratorCSharp()
